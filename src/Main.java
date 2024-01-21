fb0704f9a41b2f68bd8da63966214fc4113e3634
Денис Колесников <73371887@mail.ru>
2024-01-21 21:57:36 +0300

    homework6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello!");
+        System.out.println("homework");
+        //1
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        //2
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+        //3
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println("Четные числа " + i);
+        }
+        //4
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        //5
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println("Високосный год " + i);
+        }
+        //6
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
+        //7
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(+i);
+        }
+        //8
+        int salary = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + salary;
+            System.out.println(total);
+        }
+        //9
+        for (int i = 0; i < 12; i++) {
+            total = total + total/100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println(total);
+
+        //10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * 2);
+        }
     }
 }
